89807c3c5427bb85fa02aa7addc999ad2f2091ed
Flavio <flaviocampelo@hotmail.com>
2022-07-07 21:57:19 -0400

    Adding mongoDB extension

---
+++ ODataHelper.MongoDB/Extensions/MongoCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ODataHelper.MongoDB.Extensions
+{
+    public static class MongoCollectionExtensions
+    {
+        public static async Task<IEnumerable<T>> GetAllAsync<T>(this IMongoCollection<T> mongoCollection, IQueryCollection queryCollection)
+            where T : class
+        {
+            MongoDBQueryRunner<T> runner = new();
+            ODataParser<T> docQuery = new();
+            DocumentQuery<T> query = docQuery.TryParse(queryCollection);
+            runner.Create(query);
+            IList<T>? list = await runner.QueryAsync(mongoCollection);
+            return list;
+        }
+    }
+}
--- ODataHelper.MongoDB/Usings.cs
+++ ODataHelper.MongoDB/Usings.cs
@@ -1,3 +1,6 @@
-﻿global using MongoDB.Driver;
+﻿global using Microsoft.AspNetCore.Http;
+global using MongoDB.Driver;
+global using ODataQueryHelper.Core;
 global using ODataQueryHelper.Core.Model;
+global using ODataQueryRunner.MongoDB;
 global using System.Linq.Expressions;
