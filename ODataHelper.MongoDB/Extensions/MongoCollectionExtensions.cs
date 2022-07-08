63a9cbe3e70ff4d3561d76a6730e780db1271074
Flavio Campelo <fcampelo@biron.ca>
2022-07-08 13:31:38 -0400

    extensions with enumerable of strings

--- ODataHelper.MongoDB/Extensions/MongoCollectionExtensions.cs
+++ ODataHelper.MongoDB/Extensions/MongoCollectionExtensions.cs
@@ -2,12 +2,15 @@
 {
     public static class MongoCollectionExtensions
     {
-        public static async Task<IEnumerable<T>> GetAllAsync<T>(this IMongoCollection<T> mongoCollection, IQueryCollection queryCollection)
+        public static Task<IEnumerable<T>> GetAllAsync<T>(this IMongoCollection<T> mongoCollection, IQueryCollection queryCollection)
+            where T : class => mongoCollection.GetAllAsync(queryCollection.ToList().Select(x => new KeyValuePair<string, string>(x.Key, x.Value.ToString())));
+
+        public static async Task<IEnumerable<T>> GetAllAsync<T>(this IMongoCollection<T> mongoCollection, IEnumerable<KeyValuePair<string, string>> queryList)
             where T : class
         {
             MongoDBQueryRunner<T> runner = new();
             ODataParser<T> docQuery = new();
-            DocumentQuery<T> query = docQuery.TryParse(queryCollection);
+            DocumentQuery<T> query = docQuery.TryParse(queryList);
             runner.Create(query);
             IList<T>? list = await runner.QueryAsync(mongoCollection);
             return list;
